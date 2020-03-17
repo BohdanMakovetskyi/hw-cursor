0da117c2220c023a4b9733c6b553f0baf3412c0d
Bohdan <bohdan.makovetskyi@gmail.com>
2020-03-17 10:49:56 +0200

    Home-work 12

--- hw-cursor 12/main.js
+++ hw-cursor 12/main.js
@@ -1,45 +1,69 @@
 const BASE = 'https://swapi.co/api/';
 
-async function getPersons(film) {
+async function getPersons() {
     try {
-        const response = await axios.get(BASE + `films/${film}/`);
-        let persons = [];
-        const api = response.data.characters;
-        api.forEach(url => {
-            async function f() {
-                const person = await axios.get(url);
-                persons.push(person.data);
-            }
-            f();
-        });
-        return persons;
+        const film = document.querySelector('.inputFilm').value;
+        if(parseInt(film)>0 && parseInt(film)<=7){
+            const response = await axios.get(BASE + `films/${film}/`);
+            const api = response.data.characters;
+            let container = document.querySelector('.persons');
+            container.innerHTML = '';
+            api.forEach(url => {
+                async function f() {
+                    let person = await axios.get(url);
+                    let personDiv = document.createElement('div');
+                    personDiv.innerHTML = `
+                        <img src="./img/stormtrooper.png">
+                        <h4>${person.data.name}3</h4>
+                        <h5>${person.data.birth_year}</h5>
+                        <h5>${person.data.gender}</h5>
+                   `;
+                    personDiv.classList.add('person');
+                    container.append(personDiv);
+                }
+                f();
+            });
+        } else {
+            alert('Введіть номер фільму;)');
+        }
     } catch (e) {
         console.log(e);
     }
 
 }
 
-function renderPersons() {
-    const  film = document.querySelector('.inputFilm').value;
-    if(parseInt(film)>0 && parseInt(film)<=7){
-        getPersons(film).then(persons => {
-            let container = document.querySelector('.persons');
-            container.innerHTML = '';
-            persons.forEach(person => {
-               let personDiv = document.createElement('div');
-               personDiv.innerHTML = `
-                    <h4>${person.name}</h4>
-                    <h3>${person.birth_year}</h3>
-                    <h3>${person.gender}</h3>
-               `;
-               personDiv.classList.add('person');
-               container.append(personDiv);
-            });
-        });
-    } else {
-        alert('Введіть номер фільму;)');
+async function getPlanets(api = `${BASE}planets/?page=1`) {
+    try {
+        const response = await axios.get(api);
+        let planets = response.data.results;
+        let container = document.querySelector('.planets');
+        container.innerHTML = '';
+        for (let i = 0; i < planets.length; i++){
+            let planet = document.createElement('h3');
+            planet.innerHTML = `
+                ${i+1}: ${planets[i].name}
+            `;
+            container.append(planet);
+        }
+    } catch (e) {
+        console.log(e);
     }
 }
+getPlanets();
 
-document.querySelector('.info').addEventListener('click', renderPersons);
-
+let i = 1;
+document.querySelector('.next').addEventListener('click', function () {
+    i++;
+    if(i === 8){
+        i = 7;
+    }
+    getPlanets(BASE + `planets/?page=${i}`);
+    document.querySelector('.back').addEventListener('click', function () {
+        i--;
+        if(i === 0) {
+            i = 1;
+        }
+        getPlanets(BASE + `planets/?page=${i}`);
+    });
+});
+document.querySelector('.info').addEventListener('click', getPersons);
