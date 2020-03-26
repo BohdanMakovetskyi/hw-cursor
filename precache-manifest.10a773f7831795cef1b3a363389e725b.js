5259a36e93012477fcf8dc0ab1104145f0ebfebc
Bohdan <bohdan.makovetskyi@gmail.com>
2020-03-26 17:25:47 +0200

    Updates

---
+++ precache-manifest.10a773f7831795cef1b3a363389e725b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b61a95ff2f5da91653a587b17027891",
+    "url": "/BohdanMakovetskyi/hw-cursor/tree/master/hw-cursor_15/my-app/index.html"
+  },
+  {
+    "revision": "22eb62c9f385713f085f",
+    "url": "/BohdanMakovetskyi/hw-cursor/tree/master/hw-cursor_15/my-app/static/css/main.3a498260.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/BohdanMakovetskyi/hw-cursor/tree/master/hw-cursor_15/my-app/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BohdanMakovetskyi/hw-cursor/tree/master/hw-cursor_15/my-app/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22eb62c9f385713f085f",
+    "url": "/BohdanMakovetskyi/hw-cursor/tree/master/hw-cursor_15/my-app/static/js/main.a7d50e37.chunk.js"
+  },
+  {
+    "revision": "68196ddcd0b90ffea22e",
+    "url": "/BohdanMakovetskyi/hw-cursor/tree/master/hw-cursor_15/my-app/static/js/runtime-main.0acdd643.js"
+  }
+]);
